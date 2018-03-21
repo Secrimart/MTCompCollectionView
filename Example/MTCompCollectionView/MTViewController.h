e634d7c2c48b4812108354bdc5f17b0a3eb0bca7
Secrimart <secrimart@aliyun.com>
2018-03-21 11:36:58 +0800

    Initial commit

---
+++ Example/MTCompCollectionView/MTViewController.h
@@ -0,0 +1,13 @@
+//
+//  MTViewController.h
+//  MTCompCollectionView
+//
+//  Created by rstx_reg@aliyun.com on 03/21/2018.
+//  Copyright (c) 2018 rstx_reg@aliyun.com. All rights reserved.
+//
+
+@import UIKit;
+
+@interface MTViewController : UIViewController
+
+@end
